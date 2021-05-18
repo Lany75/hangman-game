330332323adc830d0be9f12695346dfa638678df
Lany75 <mlanie.parry@gmail.com>
2021-05-18 10:11:41 +0200

    F1 - get random word

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import SelectedWord from './components/selectedWord/SelectedWord';
 
 function App() {
   return (
     <div className="App">
       Mon appli
+      <SelectedWord />
     </div>
   );
 }
