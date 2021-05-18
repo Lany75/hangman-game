e116a4d6a8806fee653875095b590e6dab1dd63b
Lany75 <mlanie.parry@gmail.com>
2021-05-18 15:42:30 +0200

    F4 - CSS SelectedWord component

--- src/components/selectedWord/SelectedWord.js
+++ src/components/selectedWord/SelectedWord.js
@@ -1,14 +1,23 @@
-import React from 'react';
 import wordList from '../../datas/word-french';
 import getRandomInteger from '../../utils/getRandomInteger';
-import UnderscoreWord from '../underscoreWord/UnderscoreWord';
+import './SelectedWord.css';
 
 const SelectedWord = () => {
-  const randomWord = wordList[getRandomInteger(wordList.length)];
+  const randomWord = wordList[getRandomInteger(wordList.length)].word;
+  const letters = randomWord.split('');
+  const letterRegex = /[a-zàáâãäåçèéêëìíîïðòóôõöùúûüýÿ]/
 
   return (
-    <div>
-      <UnderscoreWord word={randomWord.word} />
+    <div className='selected-word'>
+      {letters.map((l, index) => {
+        if (letterRegex.test(l)) {
+          return <div className='word-letter' key={index}>_</div>
+        }
+        else {
+          if (l === ' ') return <div className='word-letter' key={index}>&nbsp;</div>
+          else return <div className='word-letter' key={index}>{l}</div>
+        }
+      })}
     </div>
   )
 }
