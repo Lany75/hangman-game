0398fa949246fbb8788e99ede2f4c0dfb46495b4
Lany75 <mlanie.parry@gmail.com>
2021-05-19 16:38:52 +0200

    F9 - Bad letter proposed

--- src/context/gallowsContext.js
+++ src/context/gallowsContext.js
@@ -4,10 +4,11 @@ import potence0 from '../img/potence0.jpg';
 export const GallowsContext = createContext();
 
 function GallowsProvider({ children }) {
+  const [errorCounter, setErrorCounter] = useState(0);
   const [image, setImage] = useState(potence0);
 
   return (
-    <GallowsContext.Provider value={{ image, setImage }}>
+    <GallowsContext.Provider value={{ errorCounter, setErrorCounter, image, setImage }}>
       {children}
     </GallowsContext.Provider>
   )
