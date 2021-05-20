974ed7890485344b117b0d010b4e5a967829a0ad
Lany75 <mlanie.parry@gmail.com>
2021-05-20 14:49:26 +0200

    F15 - Delete commented code

--- src/components/selectedWord/SelectedWord.js
+++ src/components/selectedWord/SelectedWord.js
@@ -9,13 +9,6 @@ const SelectedWord = () => {
     <div className='selected-word'>
       {underscoredWord && underscoredWord.map((l, index) => {
         return <div className='word-letter' key={index}>{l}</div>
-        /*if (letterRegex.test(l)) {
-          return <div className='word-letter' key={index}>_</div>
-        }
-        else {
-          if (l === ' ') return <div className='word-letter' key={index}>&nbsp;</div>
-          else return <div className='word-letter' key={index}>{l}</div>
-        }*/
       })}
     </div>
   )
