5843c1312c44e6a4ad27fcf41b7954be165c5d69
Lany75 <mlanie.parry@gmail.com>
2021-05-20 14:18:21 +0200

    F12 - Develop replay game button

---
+++ src/components/replayButton/ReplayButton.js
@@ -0,0 +1,29 @@
+import { useContext } from 'react';
+import { GallowsContext } from '../../context/gallowsContext';
+import { WordContext } from '../../context/wordContext';
+import potence0 from '../../img/potence0.jpg';
+import './ReplayButton.css';
+
+const ReplayButton = () => {
+  const { getRandomWord } = useContext(WordContext);
+  const { setErrorCounter, setImage } = useContext(GallowsContext);
+
+  const replay = () => {
+    setImage(potence0);
+    setErrorCounter(0);
+
+    const buttonLetter = document.getElementsByClassName('letter-button');
+    for (let i = 0; i < buttonLetter.length; i++) {
+      buttonLetter[i].disabled = false;
+    }
+
+    getRandomWord();
+
+    const buttonReplay = document.getElementById('replay-button');
+    buttonReplay.style.display = 'none';
+  }
+
+  return <button id='replay-button' onClick={replay}>REJOUER</button>
+}
+
+export default ReplayButton;
--- src/components/selectedWord/SelectedWord.js
+++ src/components/selectedWord/SelectedWord.js
@@ -3,19 +3,19 @@ import { WordContext } from '../../context/wordContext';
 import './SelectedWord.css';
 
 const SelectedWord = () => {
-  const { word } = useContext(WordContext);
-  const letterRegex = /[A-Z]/
+  const { underscoredWord } = useContext(WordContext);
 
   return (
     <div className='selected-word'>
-      {word && word.map((l, index) => {
-        if (letterRegex.test(l)) {
+      {underscoredWord && underscoredWord.map((l, index) => {
+        return <div className='word-letter' key={index}>{l}</div>
+        /*if (letterRegex.test(l)) {
           return <div className='word-letter' key={index}>_</div>
         }
         else {
           if (l === ' ') return <div className='word-letter' key={index}>&nbsp;</div>
           else return <div className='word-letter' key={index}>{l}</div>
-        }
+        }*/
       })}
     </div>
   )
