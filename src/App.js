da537d6a1198950268388d66e4ebe8a4e08f8dcc
Lany75 <mlanie.parry@gmail.com>
2021-05-25 17:17:37 +0200

    F19 - Display score

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Goal from './components/goal/Goal';
 import Messages from './components/messages/Messages';
 import ProposedLetters from './components/proposedLetters/ProposedLetters';
 import ReplayButton from './components/replayButton/ReplayButton';
+import Score from './components/score/Score';
 import SelectedWord from './components/selectedWord/SelectedWord';
 import GallowsProvider from './context/gallowsContext';
 import MessagesProvider from './context/messagesContext';
+import ScoreProvider from './context/scoreContext';
 import WordProvider from './context/wordContext';
 
 function App() {
@@ -16,18 +18,21 @@ function App() {
     <WordProvider>
       <GallowsProvider>
         <MessagesProvider>
-          <div className="App">
-            <Goal />
-            <div className='word-message'>
-              <SelectedWord />
-              <Messages />
+          <ScoreProvider>
+            <div className="App">
+              <Goal />
+              <Score />
+              <div className='word-message'>
+                <SelectedWord />
+                <Messages />
+              </div>
+              <Gallows />
+              <div className='letters-replay'>
+                <ProposedLetters />
+                <ReplayButton />
+              </div>
             </div>
-            <Gallows />
-            <div className='letters-replay'>
-              <ProposedLetters />
-              <ReplayButton />
-            </div>
-          </div>
+          </ScoreProvider>
         </MessagesProvider>
       </GallowsProvider>
     </WordProvider>
