86f56870af221cc13b393a1fc7fc6b6c6c32200e
Lany75 <mlanie.parry@gmail.com>
2021-05-28 16:50:16 +0200

    F25 - SelectedWord test

--- src/tests/SelectedWord.test.js
+++ src/tests/SelectedWord.test.js
@@ -13,18 +13,40 @@ describe('<SelectedWord />', () => {
     React.useContext = realUseContext;
   });
 
-  it('should show the SelectedWord component', () => {
+  it('should contain a div witch className is selected-word', () => {
     jest.spyOn(React, 'useContext').mockImplementation(() => ({
-      underscoredWord: ['_', '_', '_', '_', '_'] // correspond à "essai"
+      underscoredWord: ['_', '_', '_', '_', '_']
     }));
 
     const selectedWord = shallow(<SelectedWord />);
     expect(selectedWord.find('.selected-word')).to.have.length(1);
+  })
+
+  it('should show the SelectedWord component with 5 letters', () => {
+    jest.spyOn(React, 'useContext').mockImplementation(() => ({
+      underscoredWord: ['_', '_', '_', '_', '_']
+    }));
+
+    const selectedWord = shallow(<SelectedWord />);
 
     const wordLetter = selectedWord.find('.word-letter');
     expect(wordLetter).to.have.length(5);
     wordLetter.map((wl) => {
       expect(wl.text()).to.equal('_')
+    });
+  });
+
+  it('should show the SelectedWord component with 3 letters', () => {
+    jest.spyOn(React, 'useContext').mockImplementation(() => ({
+      underscoredWord: ['Z', 'Z', 'Z']
+    }));
+
+    const selectedWord = shallow(<SelectedWord />);
+
+    const wordLetter = selectedWord.find('.word-letter');
+    expect(wordLetter).to.have.length(3);
+    wordLetter.map((wl) => {
+      expect(wl.text()).to.equal('Z');
     });
   });
 })
