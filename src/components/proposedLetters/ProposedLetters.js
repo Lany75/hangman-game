7b517c265b132be05f0bc6daec2c70dd99fcb5a8
Lany75 <mlanie.parry@gmail.com>
2021-05-28 16:51:24 +0200

    F25 - ProposedLetters test

--- src/components/proposedLetters/ProposedLetters.js
+++ src/components/proposedLetters/ProposedLetters.js
@@ -1,9 +1,8 @@
 import './ProposedLetters.css';
 import Letter from '../letter/Letter';
+import ALPHABET from '../../utils/alphabet';
 
 const ProposedLetters = () => {
-  const ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-
   return (
     <div className='proposed-letters'>
       {ALPHABET.map((letter, index) => {
