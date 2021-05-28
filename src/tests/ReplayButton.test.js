32abc18906a553ff42ecade70db3fa20dd0de1a0
Lany75 <mlanie.parry@gmail.com>
2021-05-28 16:49:45 +0200

    F25 - ReplayButton test

--- src/tests/ReplayButton.test.js
+++ src/tests/ReplayButton.test.js
@@ -13,15 +13,41 @@ describe('<ReplayButton/>', () => {
     React.useContext = realUseContext;
   })
 
-  it('should show the replayButton component', () => {
+  it('should show the replayButton component when isActiveReplayButton is false', () => {
     jest.spyOn(React, 'useContext').mockImplementation(() => ({
       getRandomWord: jest.fn(),
       setErrorCounter: jest.fn(),
-      setImage: jest.fn()
+      setImage: jest.fn(),
+      setIsWon: false,
+      setIsLost: false,
+      isActiveReplayButton: false,
+      setIsActiveReplayButton: jest.fn(),
+      setAreDisabledProposedLetters: jest.fn()
     }));
 
     const replayButton = shallow(<ReplayButton />);
 
     expect(replayButton.find('#replay-button')).to.have.length(1);
+    expect(replayButton.props().disabled).to.equal(true);
+    expect(replayButton.text()).to.equal('REJOUER');
+  });
+
+  it('should show the replayButton component when isActiveReplayButton is true', () => {
+    jest.spyOn(React, 'useContext').mockImplementation(() => ({
+      getRandomWord: jest.fn(),
+      setErrorCounter: jest.fn(),
+      setImage: jest.fn(),
+      setIsWon: jest.fn(),
+      setIsLost: jest.fn(),
+      isActiveReplayButton: true,
+      setIsActiveReplayButton: jest.fn(),
+      setAreDisabledProposedLetters: jest.fn()
+    }));
+
+    const replayButton = shallow(<ReplayButton />);
+
+    expect(replayButton.find('#replay-button')).to.have.length(1);
+    expect(typeof (replayButton.props().onClick)).to.equal('function');
+    expect(replayButton.text()).to.equal('REJOUER');
   });
 });
